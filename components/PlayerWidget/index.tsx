057f08f6f9b7a19cac389c8e9ade74a30279f5d2
raynaud0 <bolivez@hotmail.com>
2020-12-19 23:36:27 +0100

    pausing and playing mp3 using expo-av

--- components/PlayerWidget/index.tsx
+++ components/PlayerWidget/index.tsx
@@ -1,18 +1,52 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Text, Image, View, TouchableOpacity } from "react-native";
 import { AntDesign, FontAwesome } from "@expo/vector-icons";
+import { Audio } from "expo-av";
 
 import styles from "./styles";
+import { Sound } from "expo-av/build/Audio";
 
 const song = {
   id: "1",
+  uri: "",
   imageUri: "https://images-na.ssl-images-amazon.com/images/I/61F66QURFyL.jpg",
   title: "Widget Msic",
   artist: "Ada",
 };
 
 const PlayerWidget = () => {
-  const onPlayPausePress = () => {};
+  const [sound, setSound] = useState<Sound | null>(null);
+  const [isPlaying, setIsPlaying] = useState(true);
+
+  const onPlaybackStatusUpdate = (status) => {
+    setIsPlaying(status.isPlaying);
+  };
+
+  const playCurrentSong = async () => {
+    if (sound) {
+      await sound.unloadAsync(); //! play only one song at a time
+    }
+    const { sound: newSound } = await Audio.Sound.createAsync(
+      { uri: song.uri },
+      { shouldPlay: isPlaying },
+      onPlaybackStatusUpdate
+    );
+    setSound(newSound);
+  };
+  useEffect(() => {
+    playCurrentSong();
+  }, []);
+
+  const onPlayPausePress = async () => {
+    if (!sound) {
+      return;
+    }
+    if (isPlaying) {
+      await sound.stopAsync();
+    } else {
+      await sound.playAsync();
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.row}>
@@ -27,7 +61,7 @@ const PlayerWidget = () => {
             <AntDesign name="hearto" size={25} color={"white"} />
             <TouchableOpacity onPress={onPlayPausePress}>
               <FontAwesome
-                name={1 ? "pause" : "play"}
+                name={isPlaying ? "pause" : "play"}
                 size={25}
                 color={"white"}
               />
